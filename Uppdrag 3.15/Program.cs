0681f15f3dd06c0c350011debbc225786b3e6a12
Mako05001 <112701714+Mako05001@users.noreply.github.com>
2022-09-30 13:45:02 +0200

    Update Program.cs

--- Uppdrag 3.15/Program.cs
+++ Uppdrag 3.15/Program.cs
@@ -1,20 +1,23 @@
-﻿using System;
+using System;
 namespace uppdrag3._15
 {
     class Program
     {
-        static void Main(string[]args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Hur länge vill du hyra bilen för? (Bara i heltal och inte längre än en dag)");
-            string H = Console.ReadLine(); 
-            int  Hours = Convert.ToInt32(H);
-            int pay = 80* Hours;
-            if (pay > 950)
+            string H = Console.ReadLine();
+            int Hours = Convert.ToInt32(H);
+            int pay = 80 * Hours;
+            if (pay <= 950 && Hours <= 11)
             {
-                pay = 950;
+                Console.WriteLine("Du ska hyra bilen i " + Hours + " timmar och det kommer att kosta " + pay + "Kr.");
             }
-            Console.WriteLine("Du ska hyra bilen i " + Hours +" timmar och det kommer att kosta " + pay+ "Kr.");
-            
+            else
+            {
+                Console.WriteLine("Du får ej hyra bilen så länge");
+            }
+
         }
     }
 }
